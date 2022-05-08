a6dbe529d530052e54e8ddb30823a70db57d6c04
shubham pandav <shubhampandav1479@gmail.com>
2022-05-08 21:03:04 +0530

    Initial commit

---
+++ app/src/main/java/com/shubhampandav/cryptoapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.shubhampandav.cryptoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.fragment.findNavController
+import com.shubhampandav.cryptoapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater )
+        setContentView(binding.root)
+
+        val navHostFragment  = supportFragmentManager.findFragmentById(R.id.fragment_container_view)
+        val navController = navHostFragment!!.findNavController()
+
+        val popupMenu = PopupMenu(this,null)
+        popupMenu.inflate(R.menu.bottomnavmenu)
+        binding.bottomBar.setupWithNavController(popupMenu.menu, navController)
+    }
+}
