5573b7a9e1a3b173e2ff5c3bc9b8f93e64ddeeac
shubham pandav <shubhampandav1479@gmail.com>
2022-05-09 18:08:44 +0530

    ui done

---
+++ app/src/main/java/com/shubhampandav/cryptoapp/model/Data.kt
@@ -0,0 +1,6 @@
+package com.shubhampandav.cryptoapp.model
+
+data class Data(
+    val cryptoCurrencyList: List<CryptoCurrency>,
+    val totalCount: String
+)
---
+++ app/src/main/java/com/shubhampandav/cryptoapp/model/marketmodel.kt
@@ -0,0 +1,6 @@
+package com.shubhampandav.cryptoapp.model
+
+data class marketmodel(
+    val `data`: Data,
+    val status: Status
+)
